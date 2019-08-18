447225f11a2a46a56077193b6f0c759ad9f133a3
sxz799 <songxianzhi521@gmail.com>
2019-08-18 17:14:37 +0800

    添加20E金元宝充值判断

--- python/AutoCharge.py
+++ python/AutoCharge.py
@@ -8,7 +8,7 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 localtime=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
-str="\n%s 充值详情：任何使用问题请联系QQ:1102041547\nVersion=1.1\n"%(localtime)
+str="\n任何使用问题请联系QQ:1102041547 程序当前版本V1.3\n%s 充值详情：\n"%(localtime)
 f= open("chargelog.txt","a")
 f.write(str)
 f.close() #记录帐号不存在日志
@@ -20,6 +20,7 @@ USER = cf.get("mysql-datebase", "USER")
 PASSWD = cf.get("mysql-datebase", "PASSWD")
 DB = cf.get("mysql-datebase", "DB")
 CHARGE_NUM = cf.getint("mysql-datebase", "CHARGE_NUM")
+isFullGold = cf.getint("mysql-datebase", "isFullGold")
 user_account = cf.items("USER_ACCOUNT")
 temp=len(user_account)
 db = pymysql.connect(HOST,USER,PASSWD,DB) #链接数据库
@@ -33,6 +34,8 @@ for i in range(0,temp):
   after_coin=CHARGE_NUM+before_coin #计算增加后的银元宝数量
   if after_coin<2000000000: #判断是否超过20e
    cursor.execute("UPDATE account SET silver_coin = %s, CHECKSUM = upper(CAST(md5(concat(CAST(account AS char CHARACTER SET utf8),CAST(PASSWORD AS char CHARACTER SET utf8),CAST(LPAD(CONV(privilege, 10, 16), 8, 0) AS char CHARACTER SET utf8),CAST(blocked_time AS char CHARACTER SET utf8),CAST(LPAD(CONV(gold_coin, 10, 16), 8, 0) AS char CHARACTER SET utf8),CAST(LPAD(CONV(silver_coin, 10, 16), 8, 0) AS char CHARACTER SET utf8),CAST(coin_password AS char CHARACTER SET utf8),CAST(unlock_coin_password_time AS char CHARACTER SET utf8),CAST(trade_lock_time AS char CHARACTER SET utf8),CAST(permit_ip AS char CHARACTER SET utf8),'ABCDEF')) AS CHAR))WHERE account = %s ;" ,(after_coin,user_account[i][1]))
+   if isFullGold==1: #判断是否将金元宝修改为20E
+    cursor.execute("UPDATE account SET gold_coin = 2000000000, CHECKSUM = upper(CAST(md5(concat(CAST(account AS char CHARACTER SET utf8),CAST(PASSWORD AS char CHARACTER SET utf8),CAST(LPAD(CONV(privilege, 10, 16), 8, 0) AS char CHARACTER SET utf8),CAST(blocked_time AS char CHARACTER SET utf8),CAST(LPAD(CONV(gold_coin, 10, 16), 8, 0) AS char CHARACTER SET utf8),CAST(LPAD(CONV(silver_coin, 10, 16), 8, 0) AS char CHARACTER SET utf8),CAST(coin_password AS char CHARACTER SET utf8),CAST(unlock_coin_password_time AS char CHARACTER SET utf8),CAST(trade_lock_time AS char CHARACTER SET utf8),CAST(permit_ip AS char CHARACTER SET utf8),'ABCDEF')) AS CHAR))WHERE account = %s ;" ,(user_account[i][1]))
    db.commit()
    str1="   帐号 %-8s 成功充值%s银元宝，当前银元宝为 %s\n"%(user_account[i][1],CHARGE_NUM,after_coin)
    f= open("chargelog.txt","a")
